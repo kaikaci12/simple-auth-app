6822e1369c1f78e4dca5bdf568725d1b1afed011
kaikaci12 <somewonesomewone@gmail.com>
2025-02-03 17:54:35 +0400

    add login logout register functions

--- app/context/AuthContext.ts
+++ app/context/AuthContext.ts
@@ -3,24 +3,37 @@ import {
   createContext,
   type PropsWithChildren,
   useState,
+  useEffect,
 } from "react";
-
+interface AuthProps {
+  authState?: { token: string | null; authenticated: boolean | null };
+  onRegister?: (email: string, password: string) => Promise<any>;
+  onLogin?: (email: string, password: string) => Promise<any>;
+  onLogout?: () => Promise<any>;
+}
+import SecureStore from "expo-secure-store";
 const TOKEN_KEY = "my-jwt";
 export const API_URL = "http://localhost:3000";
 
-const AuthContext = createContext<{
-  signIn: () => void;
-  signOut: () => void;
-  session?: string | null;
-  isLoading: boolean;
-}>({
-  signIn: () => null,
-  signOut: () => null,
-  session: null,
-  isLoading: false,
+const AuthContext = createContext<AuthProps>({
+  onLogin,
+  authState,
+  onLogout,
+  onRegister,
 });
+
 const useAuth = () => useContext(AuthContext);
+
 export const AuthProvider = ({ children }: PropsWithChildren<{}>) => {
+  useEffect(() => {
+    const loadToken = async () => {
+      const token = await SecureStore.getItemAsync(TOKEN_KEY);
+      if (token) {
+      }
+    };
+    loadToken();
+  }, []);
+
   const [authState, setAuthState] = useState<{
     token: string | null;
     authenticated: boolean | null;
@@ -29,54 +42,27 @@ export const AuthProvider = ({ children }: PropsWithChildren<{}>) => {
     authenticated: null,
   });
 
-  const register = () => {};
-  const login = () => {};
-  const logOut = () => {};
-  return (
-    <AuthContext.Provider
-      value={{
-        signIn: login,
-        signOut: logOut,
-        session: null,
-        isLoading: false,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
+  const register = (email: string, password: string) => {};
+  const login = async (email: string, password: string) => {
+    setAuthState({
+      token: "data",
+      authenticated: true,
+    });
+    console.log("user logged in succesfully");
+    await SecureStore.setItemAsync(TOKEN_KEY, "data");
+  };
+  const logOut = async () => {
+    setAuthState({
+      token: null,
+      authenticated: false,
+    });
+    console.log("user logged out in succesfully");
+    await SecureStore.deleteItemAsync(TOKEN_KEY);
+  };
+  const value = {
+    onRegister: register,
+    onLogIn: login,
+    onLogOut: logOut,
+    authState,
+  };
 };
-
-// // This hook can be used to access the user info.
-// export function useSession() {
-//   const value = useContext(AuthContext);
-//   if (process.env.NODE_ENV !== "production") {
-//     if (!value) {
-//       throw new Error("useSession must be wrapped in a <SessionProvider />");
-//     }
-//   }
-
-//   return value;
-// }
-
-// export function SessionProvider({ children }: PropsWithChildren) {
-//   const [session, setSession] = useStorageState("session");
-//   const isLoading = session === undefined;
-
-//   return (
-//     <AuthContext.Provider
-//       value={   signIn: () => {
-
-//         setSession("xxx");
-//       },
-//       signOut: () => {
-//         setSession(null);
-//       },
-//       session,
-//       isLoading,
-//     }}
-
-//     >
-//       {children}
-//     </AuthContext.Provider>
-//   )
-// }
